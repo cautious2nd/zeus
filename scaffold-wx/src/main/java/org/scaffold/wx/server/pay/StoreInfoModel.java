231ac5d719ed897f32623f11c525a83a5a7383a9
迟子悦 <chizf@outlook.com>
2023-06-09 08:55:31 +0800

    store

--- scaffold-wx/src/main/java/org/scaffold/wx/server/pay/StoreInfoModel.java
+++ scaffold-wx/src/main/java/org/scaffold/wx/server/pay/StoreInfoModel.java
@@ -6,10 +6,9 @@ package org.scaffold.wx.server.pay;
 public class StoreInfoModel {
 
     private String id;//    门店编号	id	string[1,32]	是	商户侧门店编号 示例值：0001
-    private String namel;//   门店名称	name	string[1,256]	否	商户侧门店名称 示例值：腾讯大厦分店
+    private String name;//   门店名称	name	string[1,256]	否	商户侧门店名称 示例值：腾讯大厦分店
     private String areaCode;//    地区编码	area_code	string[1,32]	否	地区编码，详细请见省市区编号对照表。 示例值：440305
     private String address;//   详细地址	address	string[1,512]	否	详细的商户门店地址 示例值：广东省深圳市南山区科技中一道10000号
-
 
     public String getId() {
         return id;
@@ -19,12 +18,12 @@ public class StoreInfoModel {
         this.id = id;
     }
 
-    public String getNamel() {
-        return namel;
+    public String getName() {
+        return name;
     }
 
-    public void setNamel(String namel) {
-        this.namel = namel;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getAreaCode() {
